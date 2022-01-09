70fb31e16b09c61d3beba512cf8f62ade3cb864b
Serhat Teker <serhat.teker@gmail.com>
2022-01-09 22:33:00 +0300

    Fix .fzf.zsh

--- .config/zsh/.fzf.zsh
+++ .config/zsh/.fzf.zsh
@@ -2,17 +2,17 @@
 
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/serhat/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/serhat/.fzf/bin"
+if [ -d "$HOME/.fzf/bin" ] ; then
+    path+=("$HOME/.fzf/bin")
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/serhat/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "${HOME}/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/serhat/.fzf/shell/key-bindings.zsh"
+source "${HOME}/.fzf/shell/key-bindings.zsh"
 # }}}1
 
 # CUSTOM SETTINGS {{{1
