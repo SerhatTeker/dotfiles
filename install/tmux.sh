a2aaa9107305cf397d88d8c09433246a5c97cc5d
Serhat Teker <serhat.teker@gmail.com>
2022-02-20 00:23:33 +0300

    Improve tmux install

--- install/tmux.sh
+++ install/tmux.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# vim: set ft=sh et ts=4 sw=4 sts=4:
 # ----------------------------------------------------------------------------#
 #                      _           _        _ _       _
 #                     (_)_ __  ___| |_ __ _| | |  ___| |__
@@ -10,7 +12,8 @@
 # Source: https://github.com/SerhatTeker/dotfiles
 #
 # ----------------------------------------------------------------------------#
-#
+
+
 # Bash safeties: exit on error, no unset variables, pipelines can't hide errors
 set -o errexit
 set -o nounset
@@ -22,5 +25,33 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 # shellcheck source=scripts/common.sh
 source "${ROOT}/install/common.sh"
 
+# TODO: Add function
+install_tmux() {
+    :
+}
+
+# No need: auto install tpm and plugins from .tmux.conf
 # Install tmux tpm plugin manager
-git clone https://github.com/tmux-plugins/tpm ${HOME}/.config/tmux/tpm
+tpm() {
+    target="${XDG_DATA_HOME}/tmux/plugins"
+
+    mkdir -p ${target}
+
+    git clone \
+        https://github.com/tmux-plugins/tpm \
+        "${target}/tpm/tpm"
+
+    # Reload TMUX environment so TPM is sourced:
+    tmux source "${XDG_CONFIG_HOME}/tmux/tmux.conf"
+
+    # Install plugins
+    "${target}/tpm/bin/install_plugins"
+}
+
+main() {
+    install_tmux
+}
+
+main
+
+exit 0
