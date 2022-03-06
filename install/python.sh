d6a42d817de54506bd2fdf377584e646dcb654c0
Serhat Teker <serhat.teker@gmail.com>
2022-03-06 22:19:10 +0300

    Update python.sh header

--- install/python.sh
+++ install/python.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# vim: set ft=sh et ts=4 sw=4 sts=4:
+#
 # ----------------------------------------------------------------------------#
-#                      _           _        _ _       _
-#                     (_)_ __  ___| |_ __ _| | |  ___| |__
-#                     | | '_ \/ __| __/ _` | | | / __| '_ \
-#                     | | | | \__ \ || (_| | | |_\__ \ | | |
-#                     |_|_| |_|___/\__\__,_|_|_(_)___/_| |_|
+#                             _   _                       _
+#                 _ __  _   _| |_| |__   ___  _ __    ___| |__
+#                | '_ \| | | | __| '_ \ / _ \| '_ \  / __| '_ \
+#                | |_) | |_| | |_| | | | (_) | | | |_\__ \ | | |
+#                | .__/ \__, |\__|_| |_|\___/|_| |_(_)___/_| |_|
+#                |_|    |___/
+#
 #
 # Author: Serhat Teker <serhat.teker@gmail.com>
 # Source: https://github.com/SerhatTeker/dotfiles
 #
 # ----------------------------------------------------------------------------#
-#
+
+
 # Bash safeties: exit on error, no unset variables, pipelines can't hide errors
 set -o errexit
 set -o nounset
 set -o pipefail
+
 
 # Locate the root directory
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
@@ -27,7 +34,6 @@ VERSION=${PYTHON_VERSION:-3.8}
 PYTHON="python${VERSION}"
 
 # Install {{{
-
 
 install_packages() {
     # Can't use ensurepip cause it's disabled for Ubuntu
