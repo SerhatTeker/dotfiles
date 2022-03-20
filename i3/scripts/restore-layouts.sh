06d4f2669302e4c91e35fd4c0b7a8567d33c73b9
Serhat Teker <serhat.teker@gmail.com>
2022-03-21 02:14:18 +0300

    Add restore-layouts script

---
+++ i3/scripts/restore-layouts.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# vim: set ft=sh et ts=4 sw=4 sts=4:
+
+
+# Restore i3 layouts
+# Usage:
+# exec ~/.config/i3/restore.sh
+
+
+for layout in ${HOME}/.config/i3/layouts/* ; do
+  i3-msg "workspace $(basename "${layout}" .json); append_layout ${layout}"
+done
