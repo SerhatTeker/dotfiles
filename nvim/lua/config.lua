906cca53a108d41a16e4ada2eb9cd730b0f6d230
Serhat Teker <serhat.teker@gmail.com>
2022-04-15 20:42:39 +0300

    Add vim gitsigns mappings

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -11,7 +11,7 @@ require('gitsigns').setup {
         changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
     },
     signcolumn = false, -- Toggle with `:Gitsigns toggle_signs`
-    numhl      = true,  -- Toggle with `:Gitsigns toggle_numhl`
+    numhl      = false,  -- Toggle with `:Gitsigns toggle_numhl`
     linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
     word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
     watch_gitdir = {
@@ -42,4 +42,36 @@ require('gitsigns').setup {
     yadm = {
         enable = false
     },
+    on_attach = function(bufnr)
+        local function map(mode, lhs, rhs, opts)
+            opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
+            vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+        end
+
+        -- Navigation
+        map('n', ']h', "&diff ? ']h' : '<cmd>Gitsigns next_hunk<CR>'", {expr=true})
+        map('n', '[h', "&diff ? '[h' : '<cmd>Gitsigns prev_hunk<CR>'", {expr=true})
+
+        -- Actions
+        map('n', '<leader>ht', ':Gitsigns toggle_signs<CR>')
+        map('n', '<leader>hp', ':Gitsigns preview_hunk<CR>')
+        map('v', '<leader>hp', ':Gitsigns preview_hunk<CR>')
+        map('n', '<leader>hs', ':Gitsigns stage_hunk<CR>')
+        map('v', '<leader>hs', ':Gitsigns stage_hunk<CR>')
+        map('n', '<leader>hr', ':Gitsigns reset_hunk<CR>')
+        map('v', '<leader>hr', ':Gitsigns reset_hunk<CR>')
+        map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
+        map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
+        map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
+        map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
+        map('n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
+        map('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
+        map('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
+        map('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
+        map('n', '<leader>td', '<cmd>Gitsigns toggle_deleted<CR>')
+
+        -- Text object
+        map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    end
 }
