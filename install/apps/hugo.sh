c5e472065df585d7240d5d883b12696c10fa2a39
Serhat Teker <serhat.teker@gmail.com>
2022-04-20 18:57:36 +0300

    Add hugo install script with spesific gohugo version

---
+++ install/apps/hugo.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# vim: set ft=sh et ts=4 sw=4 sts=4:
+
+
+# Install desired Gohugo to Linux or Mac ARM M1
+# Usage:
+# $ bash install-hugo
+
+
+# Bash safeties: exit on error, no unset variables, pipelines can't hide errors
+set -o errexit
+set -o nounset
+set -o pipefail
+
+
+HUGO_VERSION="0.97.0"
+
+SOURCE_DIR="/tmp/hugo_release"
+DEST_DIR="${HOME}/.local/bin"
+
+# Get OS and ARCH
+if [[ "${OSTYPE}" == "darwin"* ]]; then
+    OS_ARCH="macOS-ARM64"
+else
+    OS_ARCH="Linux-64bit"
+fi
+
+HUGO_TAR="hugo_extended_${HUGO_VERSION}_${OS_ARCH}.tar.gz"
+DOWNLOAD_URL="https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_TAR}"
+
+get_release() {
+    wget "${DOWNLOAD_URL}" -O "${SOURCE_DIR}/${HUGO_TAR}"
+    tar -xvzf "${SOURCE_DIR}/${HUGO_TAR}" -C "${SOURCE_DIR}"
+}
+
+copy_bin() {
+    cp -v "${SOURCE_DIR}/hugo" ${DEST_DIR}
+}
+
+# Remove tmp source dir
+remove_source() {
+    rm -rf ${SOURCE_DIR}
+}
+
+main() {
+    mkdir -pv ${SOURCE_DIR}
+
+    get_release
+    copy_bin
+    remove_source
+}
+
+main
