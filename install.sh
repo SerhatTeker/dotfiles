5e202b0603a637698cc98d0c1043979532a068f8
Serhat Teker <serhat.teker@gmail.com>
2022-05-03 01:59:51 +0300

    Add root install script

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# vim: set ft=sh et ts=4 sw=4 sts=4:
+# ----------------------------------------------------------------------------#
+#                      _           _        _ _       _
+#                     (_)_ __  ___| |_ __ _| | |  ___| |__
+#                     | | '_ \/ __| __/ _` | | | / __| '_ \
+#                     | | | | \__ \ || (_| | | |_\__ \ | | |
+#                     |_|_| |_|___/\__\__,_|_|_(_)___/_| |_|
+#
+# Author: Serhat Teker <serhat.teker@gmail.com>
+# Source: https://github.com/SerhatTeker/dotfiles
+#
+# Install dotfiles
+# ----------------------------------------------------------------------------#
+
+# Bash safeties: exit on error, no unset variables, pipelines can't hide errors
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Locate the root directory
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+# shellcheck source=scripts/common.sh
+source "${ROOT}/dotfiles/install/common.sh"
+
+
+main() {
+    info "Started installation"
+
+    local dir="${ROOT}/dotfiles/install"
+    bash "${dir}/base.sh"
+
+    success "Finished installation. Go build something!"
+}
+
+main
