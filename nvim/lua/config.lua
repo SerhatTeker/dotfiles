849c59730994f3748814a07fa1029d589e46d4a6
Serhat Teker <serhat.teker@gmail.com>
2022-05-08 02:29:40 +0300

    Disable icons for nvim diffview plugin

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -85,7 +85,7 @@ local cb = require'diffview.config'.diffview_callback
 require'diffview'.setup {
   diff_binaries = false,    -- Show diffs for binaries
   enhanced_diff_hl = false, -- See ':h diffview-config-enhanced_diff_hl'
-  use_icons = true,         -- Requires nvim-web-devicons
+  use_icons = false,        -- Requires nvim-web-devicons
   icons = {                 -- Only applies when use_icons is true.
     folder_closed = "",
     folder_open = "",
