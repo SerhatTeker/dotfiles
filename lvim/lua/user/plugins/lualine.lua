22dc1be0879210f2b3d224bb4cf4c2ce1434aeee
Serhat Teker <serhat.teker@gmail.com>
2022-05-22 16:11:22 +0300

    Fix lvim lualine

--- lvim/lua/user/plugins/lualine.lua
+++ lvim/lua/user/plugins/lualine.lua
@@ -1,18 +1,18 @@
-lvim.builtin.lualine = {
-  style = "default",
-  options = {
+local ll = lvim.builtin.lualine
+
+ll.style = "default"
+ll.options = {
     component_separators = { left = "", right = "" },
     section_separators = { left = "", right = "" },
     -- lightline-like
     -- component_separators = { left = '|', right = '|'},
     -- section_separators = { left = '', right = ''},
-  },
-  sections = {
+}
+ll.sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {''},
     lualine_z = {'location'}
-  }
 }
