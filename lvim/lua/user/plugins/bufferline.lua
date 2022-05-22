ba2693cb837d91a9d61f7a4c386dd62c04dbe2eb
Serhat Teker <serhat.teker@gmail.com>
2022-05-22 16:11:39 +0300

    Fix lvim bufferline

--- lvim/lua/user/plugins/bufferline.lua
+++ lvim/lua/user/plugins/bufferline.lua
@@ -1,7 +1,5 @@
-lvim.builtin.bufferline = {
-  options = {
-    indicator_icon = "",
-    buffer_close_icon = "",
-    close_icon = "",
-  }
+lvim.builtin.bufferline.options = {
+  indicator_icon = "",
+  buffer_close_icon = "",
+  close_icon = ""
 }
