b12a145704bba70377e870f591621665d62d9689
Serhat Teker <serhat.teker@gmail.com>
2022-05-22 16:12:24 +0300

    Add fugitive to lvim

--- lvim/lua/user/plugs.lua
+++ lvim/lua/user/plugs.lua
@@ -42,4 +42,24 @@ lvim.plugins = {
       require('Navigator').setup()
     end,
   },
+    -- fugitive
+    {
+      "tpope/vim-fugitive",
+      cmd = {
+        "G",
+        "Git",
+        "Gdiffsplit",
+        "Gread",
+        "Gwrite",
+        "Ggrep",
+        "GMove",
+        "GDelete",
+        "GBrowse",
+        "GRemove",
+        "GRename",
+        "Glgrep",
+        "Gedit"
+      },
+      ft = {"fugitive"}
+    },
 }
