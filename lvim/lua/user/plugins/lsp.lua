df00d281d0cfc505041e2bc5229a6d1cc136d471
Serhat Teker <serhat.teker@gmail.com>
2022-05-23 15:03:29 +0300

    Add jsx to prettier

--- lvim/lua/user/plugins/lsp.lua
+++ lvim/lua/user/plugins/lsp.lua
@@ -20,9 +20,9 @@ formatters.setup {
     command = "prettier",
     ---@usage arguments to pass to the formatter
     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-    extra_args = { "--print-with", "100" },
+    -- extra_args = { "--print-with", "100" },
     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-    filetypes = { "typescript", "typescriptreact" },
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
   },
 }
 
