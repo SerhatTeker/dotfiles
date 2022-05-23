934c95989ec803e24364f0b46eeb7c98299a5c5e
Serhat Teker <serhat.teker@gmail.com>
2022-05-23 18:47:09 +0300

    Add grouping to lvim opts

--- lvim/lua/user/options.lua
+++ lvim/lua/user/options.lua
@@ -1,4 +1,4 @@
--- # General
+-- # Core
 vim.opt.relativenumber = true -- set relative numbered lines
 vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:block,r-cr-o:hor50"
 vim.opt.cmdheight = 1
@@ -7,8 +7,7 @@ vim.opt.expandtab = true -- convert tabs to spaces
 vim.opt.tabstop = 4 -- insert 2 spaces for a tab
 vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
 vim.opt.softtabstop = 4 -- how many columns (=spaces) the cursor moves right when you press <Tab>
-
--- # Folding
+-- folding
 vim.opt.foldenable = false -- disable folding on open
 vim.opt.foldmethod = "expr" -- folding set to "expr" for treesitter based folding
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()" -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
