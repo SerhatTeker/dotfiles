bebac4aa202644a397c7feb216b67b548a375d56
Serhat Teker <serhat.teker@gmail.com>
2022-05-23 15:11:29 +0300

    Update nvim-tree
    - Disable diagnostic icons
    - Enable indentline markers

--- lvim/lua/user/plugins/nvim-tree.lua
+++ lvim/lua/user/plugins/nvim-tree.lua
@@ -1,25 +1,11 @@
 -- Nvimtree
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 1
-
--- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
-}
-
-lvim.builtin.treesitter.ignore_install = { "haskell" }
-lvim.builtin.treesitter.highlight.enabled = true
+local nt = lvim.builtin.nvimtree
+nt.setup.view.side = "left"
+nt.show_icons.git = 1
+-- indent markers
+nt.setup.renderer.indent_markers.enable = true
+-- diagnostics icons
+nt.setup.diagnostics.enable = false
 
 lvim.keys.normal_mode["<C-t>"] = "<CMD>NvimTreeToggle<CR>"
 lvim.keys.normal_mode["<leader>v"] = "<CMD>NvimTreeFindFile<CR>"
