3c3e5f371956a7a4741ac924eb199dedb88494a6
Serhat Teker <serhat.teker@gmail.com>
2022-05-23 18:44:21 +0300

    Add tab/space opt to lvim

--- lvim/lua/user/options.lua
+++ lvim/lua/user/options.lua
@@ -2,6 +2,11 @@
 vim.opt.relativenumber = true -- set relative numbered lines
 vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:block,r-cr-o:hor50"
 vim.opt.cmdheight = 1
+-- tabs, spaces
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.tabstop = 4 -- insert 2 spaces for a tab
+vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
+vim.opt.softtabstop = 4 -- how many columns (=spaces) the cursor moves right when you press <Tab>
 
 -- # Folding
 vim.opt.foldenable = false -- disable folding on open
@@ -13,12 +18,12 @@ lvim.log.level = "warn"
 lvim.use_icons = true
 -- Format files on save
 lvim.format_on_save = {
-  pattern = {
-    "*.lua",
-    "*.sh",
-    "*.vim",
-    "*.go",
-    "*.js",
-    "*.jsx",
-  },
+    pattern = {
+        "*.lua",
+        "*.sh",
+        "*.vim",
+        "*.go",
+        "*.js",
+        "*.jsx",
+    },
 }
