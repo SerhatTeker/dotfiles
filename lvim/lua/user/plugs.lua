bc3b52a60aee9de1da51bd8894029d968690f1d0
Serhat Teker <serhat.teker@gmail.com>
2022-05-23 18:32:55 +0300

    Add trim plugin to lvim

--- lvim/lua/user/plugs.lua
+++ lvim/lua/user/plugs.lua
@@ -72,4 +72,19 @@ lvim.plugins = {
   -- Tokyonight
   { "folke/tokyonight.nvim" },
   { "ThePrimeagen/harpoon" },
+  {
+    "cappyzawa/trim.nvim",
+    config = function()
+      require('trim').setup({
+        disable = { "markdown" },
+
+        patterns = {
+          [[%s/\s\+$//e]], -- remove unwanted spaces
+          [[%s/\($\n\s*\)\+\%$//]], -- trim last line
+          [[%s/\%^\n\+//]], -- trim first line
+          -- [[%s/\(\n\n\)\n\+/\1/]], -- replace multiple blank lines with a single line
+        },
+      })
+    end,
+  },
 }
