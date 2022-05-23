9a52f03e7da9f5da03b9df71140914dd0410eb70
Serhat Teker <serhat.teker@gmail.com>
2022-05-23 17:47:33 +0300

    Update folding to treesitter

--- lvim/lua/user/options.lua
+++ lvim/lua/user/options.lua
@@ -3,8 +3,12 @@ vim.opt.relativenumber = true -- set relative numbered lines
 vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:block,r-cr-o:hor50"
 vim.opt.cmdheight = 1
 
+-- # Folding
+vim.opt.foldenable = false  -- disable folding on open
+vim.opt.foldmethod = "expr" -- folding set to "expr" for treesitter based folding
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()" -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
+
 -- # LunarVim
--- ## Core
 lvim.log.level = "warn"
 lvim.format_on_save = false
 lvim.use_icons = true
