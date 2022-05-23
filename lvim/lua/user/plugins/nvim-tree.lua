008ee1b80d5ba2d410dcfff9d6a925d931be483b
Serhat Teker <serhat.teker@gmail.com>
2022-05-23 16:10:03 +0300

    Add nvim-tree ignore directories

--- lvim/lua/user/plugins/nvim-tree.lua
+++ lvim/lua/user/plugins/nvim-tree.lua
@@ -7,5 +7,17 @@ nt.setup.renderer.indent_markers.enable = true
 -- diagnostics icons
 nt.setup.diagnostics.enable = false
 
+-- Ingore/Exclude directories/files patterns
+-- https://github.com/kyazdani42/nvim-tree.lua/issues/824
+nt.setup.filters.custom = {
+  ".mypy_cache",
+  "__pycache__",
+  ".pytest_cache",
+  "htmlcov",
+  "hypothesis",
+  "node_modules",
+  "\\.cache",
+}
+
 lvim.keys.normal_mode["<C-t>"] = "<CMD>NvimTreeToggle<CR>"
 lvim.keys.normal_mode["<leader>v"] = "<CMD>NvimTreeFindFile<CR>"
