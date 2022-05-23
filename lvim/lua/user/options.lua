87d5122bb091032c1311522cbf8cf05591e656aa
Serhat Teker <serhat.teker@gmail.com>
2022-05-23 18:33:16 +0300

    Add format on_save files to lvim

--- lvim/lua/user/options.lua
+++ lvim/lua/user/options.lua
@@ -4,11 +4,21 @@ vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:block,r-cr-o:hor50"
 vim.opt.cmdheight = 1
 
 -- # Folding
-vim.opt.foldenable = false  -- disable folding on open
+vim.opt.foldenable = false -- disable folding on open
 vim.opt.foldmethod = "expr" -- folding set to "expr" for treesitter based folding
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()" -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
 
 -- # LunarVim
 lvim.log.level = "warn"
-lvim.format_on_save = false
 lvim.use_icons = true
+-- Format files on save
+lvim.format_on_save = {
+  pattern = {
+    "*.lua",
+    "*.sh",
+    "*.vim",
+    "*.go",
+    "*.js",
+    "*.jsx",
+  },
+}
