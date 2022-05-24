26f7974fe95b9d6771163f4b20aa866581ab50c7
Serhat Teker <serhat.teker@gmail.com>
2022-05-24 17:46:17 +0300

    Add SymbolsOutline to which-key

--- lvim/lua/user/plugins/which-key.lua
+++ lvim/lua/user/plugins/which-key.lua
@@ -2,27 +2,30 @@
 local wk = lvim.builtin.which_key
 
 wk.opts = {
-      mode = "n", -- NORMAL mode
-      prefix = "<space>",
-      buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-      silent = true, -- use `silent` when creating keymaps
-      noremap = true, -- use `noremap` when creating keymaps
-      nowait = true, -- use `nowait` when creating keymaps
+    mode = "n", -- NORMAL mode
+    prefix = "<space>",
+    buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+    silent = true, -- use `silent` when creating keymaps
+    noremap = true, -- use `noremap` when creating keymaps
+    nowait = true, -- use `nowait` when creating keymaps
 }
 wk.vopts = {
-      mode = "v", -- VISUAL mode
-      prefix = "<space>",
-      buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-      silent = true, -- use `silent` when creating keymaps
-      noremap = true, -- use `noremap` when creating keymaps
-      nowait = true, -- use `nowait` when creating keymaps
+    mode = "v", -- VISUAL mode
+    prefix = "<space>",
+    buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+    silent = true, -- use `silent` when creating keymaps
+    noremap = true, -- use `noremap` when creating keymaps
+    nowait = true, -- use `nowait` when creating keymaps
 }
 
--- persistence.nvim
+-- Persistence
 -- restore the session for the current directory
 wk.mappings["S"] = {
-  name = "Session",
-  c = { "<cmd>lua require('persistence').load()<cr>", "Restore last session for current dir" },
-  l = { "<cmd>lua require('persistence').load({ last = true })<cr>", "Restore last session" },
-  Q = { "<cmd>lua require('persistence').stop()<cr>", "Quit without saving session" },
+    name = "Session",
+    c = { "<cmd>lua require('persistence').load()<cr>", "Restore last session for current dir" },
+    l = { "<cmd>lua require('persistence').load({ last = true })<cr>", "Restore last session" },
+    Q = { "<cmd>lua require('persistence').stop()<cr>", "Quit without saving session" },
 }
+
+-- SymbolsOutline
+lvim.builtin.which_key.mappings["o"] = { "<cmd>SymbolsOutline<CR>", "Outline" }
