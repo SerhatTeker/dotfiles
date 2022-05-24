13db493d741086d1713f098fb0ad6746768aa11e
Serhat Teker <serhat.teker@gmail.com>
2022-05-24 18:23:25 +0300

    Add full relative file path opt to lualine

--- lvim/lua/user/plugins/lualine.lua
+++ lvim/lua/user/plugins/lualine.lua
@@ -9,10 +9,11 @@ ll.options = {
     -- section_separators = { left = '', right = ''},
 }
 ll.sections = {
-    lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff' },
-    lualine_c = { 'filename' },
-    lualine_x = { 'diagnostics' },
-    lualine_y = { 'encoding', 'fileformat', 'filetype' },
-    lualine_z = { 'location' }
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff" },
+    lualine_c = { "filename" },
+    -- lualine_c = { "%f" }, -- relative full filename
+    lualine_x = { "diagnostics" },
+    lualine_y = { "encoding", "fileformat", "filetype" },
+    lualine_z = { "location" }
 }
