d828e1f6dbcc3a9d52648246889c39237f08b76a
Serhat Teker <serhat.teker@gmail.com>
2022-05-24 15:51:45 +0300

    Replace trim plugin with the fork

--- lvim/lua/user/plugs.lua
+++ lvim/lua/user/plugs.lua
@@ -98,18 +98,12 @@ lvim.plugins = {
     -- Harpoon
     { "ThePrimeagen/harpoon" },
     -- Trim
+
     {
-        "cappyzawa/trim.nvim",
+        "SerhatTeker/trim.nvim",
+        event = "BufWritePre",
         config = function()
-            require('trim').setup({
-                disable = { "markdown" },
-                patterns = {
-                    [[%s/\s\+$//e]], -- remove unwanted spaces
-                    [[%s/\($\n\s*\)\+\%$//]], -- trim last line
-                    [[%s/\%^\n\+//]], -- trim first line
-                    -- [[%s/\(\n\n\)\n\+/\1/]], -- replace multiple blank lines with a single line
-                },
-            })
+            require('trim').setup()
         end,
     },
     -- Shade, dim InactiveWindow
