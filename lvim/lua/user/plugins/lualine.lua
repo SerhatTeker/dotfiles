c817ba259150cdd3150ca30689d0af6d3e378f40
Serhat Teker <serhat.teker@gmail.com>
2022-05-25 14:29:42 +0300

    Update lualine

--- lvim/lua/user/plugins/lualine.lua
+++ lvim/lua/user/plugins/lualine.lua
@@ -1,19 +1,81 @@
 local ll = lvim.builtin.lualine
 
+
+-- # Theme {{{
+
+-- ## Custom Onedark
+local conedark = require('lualine.themes.onedark')
+
+-- switch normal and insert mode colors
+-- local onedark_blue = conedark.insert.a.bg
+-- local onedark_green = conedark.normal.a.bg
+
+-- conedark.normal.a.bg = onedark_blue
+-- conedark.insert.a.bg = onedark_green
+-- }}}
+
+-- # Core {{{
+
 ll.style = "default"
 ll.options = {
-    component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
-    -- lightline-like
-    -- component_separators = { left = '|', right = '|'},
-    -- section_separators = { left = '', right = ''},
+    -- # Separators
+    -- ## default
+    -- section_separators = { left = "", right = "" },
+    -- component_separators = { left = "", right = "" },
+    -- ## lightline-like
+    section_separators = { left = "", right = "" },
+    component_separators = { left = "|", right = "|" },
+    -- # Theme
+    theme = conedark,
 }
+-- }}}
+
+-- # Snippets {{{
+
+-- ## Changing filename color based on modified status
+local custom_fname = require('lualine.components.filename'):extend()
+local highlight = require 'lualine.highlight'
+local default_status_colors = { saved = '#228B22', modified = '#C70039' }
+
+function custom_fname:init(options)
+    custom_fname.super.init(self, options)
+    self.status_colors = {
+        saved = highlight.create_component_highlight_group(
+            { fg = default_status_colors.saved }, 'filename_status_saved', self.options),
+        modified = highlight.create_component_highlight_group(
+            { fg = default_status_colors.modified }, 'filename_status_modified', self.options),
+    }
+    if self.options.color == nil then self.options.color = '' end
+end
+
+function custom_fname:update_status()
+    local data = custom_fname.super.update_status(self)
+    data = highlight.component_format_highlight(vim.bo.modified
+        and self.status_colors.modified
+        or self.status_colors.saved) .. data
+    return data
+end
+
+-- }}}
+
+-- # Sections {{{
+
 ll.sections = {
     lualine_a = { "mode" },
     lualine_b = { "branch", "diff" },
     lualine_c = { "filename" },
     -- lualine_c = { "%f" }, -- relative full filename
+    -- lualine_c = { custom_fname },
     lualine_x = { "diagnostics" },
-    lualine_y = { "encoding", "fileformat", "filetype" },
-    lualine_z = { "location" }
+    lualine_y = { "filetype", "fileformat", "encoding" },
+    lualine_z = { "location" },
 }
+
+-- lvim-like lualine_b
+-- ll.sections.lualine_b = {
+--     { 'branch', icon = '' },
+--     { "diff",
+--         symbols = { added = "  ", modified = " ", removed = " " },
+--     },
+-- }
+-- }}}
