f0b80fa7a99a734b0b262fcd110835d600302c6c
Serhat Teker <serhat.teker@gmail.com>
2022-06-12 18:28:52 +0300

    Add go to tree-sitter

--- lvim/lua/user/plugins/tree-sitter.lua
+++ lvim/lua/user/plugins/tree-sitter.lua
@@ -1,19 +1,20 @@
 -- Treesitter
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
+    "bash",
+    "c",
+    "css",
+    "go",
+    "java",
+    "javascript",
+    "json",
+    "lua",
+    "python",
+    "rust",
+    "typescript",
+    "tsx",
+    "yaml",
 }
 
-lvim.builtin.treesitter.ignore_install = { "haskell" }
+-- lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
