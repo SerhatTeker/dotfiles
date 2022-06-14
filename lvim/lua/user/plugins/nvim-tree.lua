f0fb26f2d1aa3d0383e3430ee3352422e0b77102
Serhat Teker <serhat.teker@gmail.com>
2022-06-14 23:42:01 +0300

    Fix git icons issue in nvim-tree for new lvim

--- lvim/lua/user/plugins/nvim-tree.lua
+++ lvim/lua/user/plugins/nvim-tree.lua
@@ -1,7 +1,7 @@
 -- Nvimtree
 local nt = lvim.builtin.nvimtree
+
 nt.setup.view.side = "left"
-nt.show_icons.git = 1
 -- indent markers
 nt.setup.renderer.indent_markers.enable = true
 -- diagnostics icons
