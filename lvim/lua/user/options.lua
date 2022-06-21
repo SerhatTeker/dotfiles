55a23a327419bf7bab10f5e517050dda0509ea44
Serhat Teker <serhat.teker@gmail.com>
2022-06-21 16:33:13 +0300

    Add zsh highlight (as bash) for lvim

--- lvim/lua/user/options.lua
+++ lvim/lua/user/options.lua
@@ -30,3 +30,11 @@ lvim.format_on_save = {
         "*.vim",
     },
 }
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "zsh",
+    callback = function()
+        -- let treesitter use bash highlight for zsh files as well
+        require("nvim-treesitter.highlight").attach(0, "bash")
+    end,
+})
