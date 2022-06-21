6c245fdb61f8ffbd0e737b8c86a2f291e30632f6
Serhat Teker <serhat.teker@gmail.com>
2022-06-21 21:54:39 +0300

    Add lvim ft indents

---
+++ lvim/indent/go.lua
@@ -0,0 +1,6 @@
+-- On pressing tab, insert 8 spaces
+vim.opt_local.expandtab = false
+-- show existing tab with 8 spaces width
+vim.opt_local.tabstop = 8
+vim.opt_local.softtabstop = 8
+vim.opt_local.shiftwidth = 8
---
+++ lvim/indent/sh.lua
@@ -0,0 +1,7 @@
+-- On pressing tab, insert 4 spaces
+vim.opt_local.expandtab = true
+-- show existing tab with 4 spaces width
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.foldmethod = "marker"
