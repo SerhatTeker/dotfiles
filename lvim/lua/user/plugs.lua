abdf4950aefd29636070b468cb94c5e491d36935
Serhat Teker <serhat.teker@gmail.com>
2022-06-21 22:53:46 +0300

    Add nvim-colorizer plugin to lvim

--- lvim/lua/user/plugs.lua
+++ lvim/lua/user/plugs.lua
@@ -129,6 +129,22 @@ lvim.plugins = {
         "simrat39/symbols-outline.nvim",
         cmd = "SymbolsOutline",
     },
+    -- Color highlighter colorizer
+    {
+        "norcalli/nvim-colorizer.lua",
+        config = function()
+            require("colorizer").setup({ "*" }, {
+                RGB = true, -- #RGB hex codes
+                RRGGBB = true, -- #RRGGBB hex codes
+                RRGGBBAA = true, -- #RRGGBBAA hex codes
+                rgb_fn = true, -- CSS rgb() and rgba() functions
+                hsl_fn = true, -- CSS hsl() and hsla() functions
+                css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+                css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+                -- mode = "background", -- Available modes: foreground, background
+            })
+        end,
+    },
     -- Treesitter
     { 'nvim-treesitter/nvim-treesitter-textobjects' },
     { "p00f/nvim-ts-rainbow" },
