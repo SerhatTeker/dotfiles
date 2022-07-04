7eb2632f714e6d69d9e7d782cab4b0622279dbb7
Serhat Teker <serhat.teker@gmail.com>
2022-07-04 15:20:55 +0300

    Add json lvim ft indent conf

---
+++ lvim/indent/json.lua
@@ -0,0 +1,6 @@
+-- On pressing tab, insert 2 spaces
+vim.opt_local.expandtab = true
+-- show existing tab with 2 spaces width
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.shiftwidth = 2
