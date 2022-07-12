fad6b7511c0dace7b0975cb79c783988e3e17e20
Serhat Teker <serhat.teker@gmail.com>
2022-07-13 02:29:46 +0300

    Remove line-lenght arg from shellcheck linter

--- lvim/lua/user/plugins/lsp.lua
+++ lvim/lua/user/plugins/lsp.lua
@@ -71,7 +71,7 @@ linters.setup {
         ---@usage arguments to pass to the formatter these cannot contain
         --whitespaces, options such as `--line-width 80` become either
         --`{'--line-width', '80'}` or `{'--line-width=80'}`
-        extra_args = { "--severity", "warning", "--line-width", line_length },
+        extra_args = { "--severity", "warning" },
     },
     {
         command = "codespell",
