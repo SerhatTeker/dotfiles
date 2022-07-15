b8df38d6e4315fadce7a4b0c92d737b1500bb46f
Serhat Teker <serhat.teker@gmail.com>
2022-07-16 01:28:28 +0300

    Add default TODO plug opts

--- lvim/lua/user/plugs.lua
+++ lvim/lua/user/plugs.lua
@@ -142,6 +142,12 @@ lvim.plugins = {
         config = function()
             require("todo-comments").setup({
                 keywords = {
+                    -- # Defaults
+                    -- TODO = { icon = " ", color = "info" },
+                    -- WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+                    -- PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+                    -- NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+                    -- # Custom
                     HACK = { icon = " ", color = "warning", alt = { "DEBUG" } },
                     TEST = { icon = " ", color = "warning",
                         alt = { "TESTING", "PASSED", "FAILED" }
