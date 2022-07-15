000af77d206a80b67e2d943785f701456ac7ec83
Serhat Teker <serhat.teker@gmail.com>
2022-07-16 00:25:38 +0300

    Update "filename" component for lualine

--- lvim/lua/user/plugins/lualine.lua
+++ lvim/lua/user/plugins/lualine.lua
@@ -76,8 +76,21 @@ if ll.style == "lvim" then
             components.diff,
         },
         lualine_c = {
-            -- components.filename,
-            { "%f%" }, -- relative path
+            {
+                'filename',
+                file_status = true, -- Displays file status (readonly status, modified status)
+                -- 0: Just the filename
+                -- 1: Relative path
+                -- 2: Absolute path
+                -- 3: Absolute path, with tilde as the home directory
+                path = 1,
+                shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+                symbols = {
+                    modified = '[+]', -- Text to show when the file is modified.
+                    readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
+                    unnamed = '[No Name]', -- Text to show for unnamed buffers.
+                }
+            },
             components.python_env,
         },
         lualine_x = {
