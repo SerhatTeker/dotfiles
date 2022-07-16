4648ccd9009774821e3245c9582fd8b962c1e274
Serhat Teker <serhat.teker@gmail.com>
2022-07-16 15:31:39 +0300

    Update diff fillchar to use diagonal like diffview.nvim

--- lvim/lua/user/options.lua
+++ lvim/lua/user/options.lua
@@ -1,20 +1,26 @@
--- # Core
-vim.opt.relativenumber = true -- set relative numbered lines
-vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:block,r-cr-o:hor50"
-vim.opt.cmdheight = 1
--- tabs, spaces
-vim.opt.expandtab = true -- convert tabs to spaces
-vim.opt.tabstop = 4 -- insert 2 spaces for a tab
-vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
-vim.opt.softtabstop = 4 -- how many columns (=spaces) the cursor moves right when you press <Tab>
--- folding
-vim.opt.foldenable = false -- disable folding on open
-vim.opt.foldmethod = "expr" -- folding set to "expr" for treesitter based folding
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()" -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
--- git diff
-vim.opt.diffopt:append { "vertical" } -- internal, filler, closeoff, vertical
+-- # Core {{{
 
--- # LunarVim
+local set = vim.opt -- set options
+
+set.relativenumber = true -- set relative numbered lines
+set.guicursor = "n-v-c-sm:block,i-ci-ve:block,r-cr-o:hor50"
+set.cmdheight = 1
+-- tabs, spaces
+set.expandtab = true -- convert tabs to spaces
+set.tabstop = 4 -- insert 2 spaces for a tab
+set.shiftwidth = 4 -- the number of spaces inserted for each indentation
+set.softtabstop = 4 -- how many columns (=spaces) the cursor moves right when you press <Tab>
+-- folding
+set.foldenable = false -- disable folding on open
+set.foldmethod = "expr" -- folding set to "expr" for treesitter based folding
+set.foldexpr = "nvim_treesitter#foldexpr()" -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
+-- git diff
+set.diffopt:append { "vertical" } -- internal, filler, closeoff, vertical
+set.fillchars = set.fillchars + 'diff:╱'
+-- }}}
+
+-- # LunarVim {{{
+
 lvim.log.level = "warn"
 lvim.use_icons = true
 lvim.transparent_window = false
@@ -36,3 +42,4 @@ vim.api.nvim_create_user_command(
     end,
     { force = true, nargs = 0 }
 )
+-- }}}
