f26a95b35d60ac6c65e172c12419eda05a41b124
Serhat Teker <serhat.teker@gmail.com>
2022-07-18 18:52:14 +0300

    Update lvim theme config

--- lvim/lua/user/theme.lua
+++ lvim/lua/user/theme.lua
@@ -1,19 +1,6 @@
 -- # Colorschemes
 
--- Define colorscheme if you don't want to use global theme
-local background = nil
-local colorscheme = nil
-
--- Defaults
--- local background = "dark"
--- local colorscheme = "neodarker"
-
--- Colorscheme options
--- local colorscheme = "neodarker" -- default
--- local colorscheme = "onedark"
--- local colorscheme = "gruvbox-material"
--- local colorscheme = "vscode"
--- local colorscheme = "tokyonight"
+local M = {}
 
 -- ## Neodarker {{{
 
@@ -22,7 +9,7 @@ local colorscheme = nil
 -- vim.colorscheme = "neodarker"
 -- lvim.builtin.lualine.options.theme = "onedark"
 
-local function init_neodarker()
+function M.init_neodarker()
     require("neodarker").setup()
     -- lvim.builtin.lualine.options.theme = "neodarker"
     lvim.builtin.lualine.options.theme = "auto"
@@ -32,7 +19,7 @@ end
 
 -- ## Onedark {{{
 
-local function init_onedark()
+function M.init_onedark()
     require('onedark').setup {
         style = "darker"
     }
@@ -50,7 +37,7 @@ end
 
 -- ## Gruvbox Material {{{
 
-local function init_gruvbox_material()
+function M.init_gruvbox_material()
     -- INFO: the configuration options should be placed before `colorscheme gruvbox-material`
     -- hard | medium | soft
     vim.api.nvim_set_var("gruvbox_material_background", "hard")
@@ -68,7 +55,7 @@ end
 -- :lua require('vscode').change_style("light")
 -- :lua require('vscode').change_style("dark")
 
-local function init_vscode()
+function M.init_vscode()
     vim.g.vscode_style = "dark"
 end
 
@@ -81,59 +68,65 @@ local function init_colorscheme(cs, bg)
     lvim.colorscheme = cs
 
     if cs == "onedark" then
-        init_onedark()
+        M.init_onedark()
     elseif cs == "gruvbox-material" then
-        init_gruvbox_material()
+        M.init_gruvbox_material()
     elseif cs == "vscode" then
-        init_vscode()
+        M.init_vscode()
     else
-        init_neodarker()
+        M.init_neodarker()
     end
 end
 
-function ChangeBackground()
-    if vim.fn.has("unix") == 1 then
-        -- default colorscheme and background
-        -- local colorscheme = "neodarker"
-        -- local background = "dark"
-        if colorscheme ~= nil and background ~= nil then
-            init_colorscheme(colorscheme, background)
-        else
+-- Global custom default colorscheme and background
+local background = nil -- light | dark
+local colorscheme = nil -- neodarker | onedark | gruvbox-material | vscode | tokyonight
 
-            -- TODO: Use func global colorscheme, background
-            if vim.fn.has("mac") == 1 then
-                local theme = vim.fn.system("defaults read -g AppleInterfaceStyle")
-                if string.find(theme, "Dark") then -- ^Dark
-                    init_colorscheme("neodarker", "dark")
-                else
-                    init_colorscheme("onedark", "light")
-                end
-            else
-                local theme = vim.fn.system("gsettings get org.gnome.desktop.interface gtk-theme")
-                -- if theme == "Yaru-light" then -- Not working
-                if string.find(theme, "light") then -- Yaru-light
-                    init_colorscheme("onedark", "light")
-                else
-                    init_colorscheme("neodarker", "dark")
-                end
-            end
+function M.ChangeBackground()
+    if vim.fn.has("unix") ~= 1 then
+        vim.notify("OS should bu UNIX", vim.log.levels.ERROR, { title = "OS Type Error" })
+        return false
+    end
+
+    -- Global custom default colorscheme and background from local
+    if colorscheme ~= nil and background ~= nil then
+        init_colorscheme(colorscheme, background)
+        return true
+    end
+
+    -- default colorscheme and background
+    local cs, bg = "neodarker", "dark"
+
+    if vim.fn.has("mac") == 1 then
+        local theme = vim.fn.system("defaults read -g AppleInterfaceStyle")
+        if string.find(theme, "Dark") == nil then -- ^Dark
+            cs, bg = "onedark", "light"
+        end
+    else
+        local theme = vim.fn.system("gsettings get org.gnome.desktop.interface gtk-theme")
+        if string.find(theme, "light") then -- Yaru-light
+            cs, bg = "onedark", "light"
         end
     end
+
+    init_colorscheme(cs, bg)
 end
 
 -- }}}
 
--- init {{{
+function M.setup()
+    --  initiate in the beginning
+    M.ChangeBackground()
 
--- TODO: Use global M = {}
---  initiate in the beginning
-ChangeBackground()
+    -- Add custom command for ChangeBackground()
+    vim.api.nvim_create_user_command("AdaptGlobalTheme", M.ChangeBackground, { force = true })
 
--- Add custom command for ChangeBackground()
-vim.api.nvim_create_user_command("AdaptGlobalTheme", ChangeBackground, { force = true })
+    vim.api.nvim_create_autocmd("Signal SIGUSR1", {
+        pattern = "*",
+        callback = M.ChangeBackground,
+        -- command = "AdaptGlobalTheme",
+    })
 
-vim.api.nvim_create_autocmd("Signal SIGUSR1", {
-    pattern = "*",
-    -- callback = ChangeBackground,
-    command = "AdaptGlobalTheme",
-})
+end
+
+return M
