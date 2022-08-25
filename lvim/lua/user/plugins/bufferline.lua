bea70f96143bb6cb205aefdcb5930af2f5147a65
Serhat Teker <serhat.teker@gmail.com>
2022-08-26 00:58:10 +0300

    Add bufferline tab_selected highlight
    Fix neodarker tab_selected visibility issue

--- lvim/lua/user/plugins/bufferline.lua
+++ lvim/lua/user/plugins/bufferline.lua
@@ -1,4 +1,6 @@
-lvim.builtin.bufferline.options = {
+local bl = lvim.builtin.bufferline
+
+bl.options = {
     -- Indicator
     -- U+2589 ▐ Right half block, this character is right aligned so the
     -- background highlight doesn't appear in the middle
@@ -10,4 +12,21 @@ lvim.builtin.bufferline.options = {
     indicator_icon = "",
     buffer_close_icon = "",
     close_icon = "",
+}
+
+bl.highlights = {
+    background = {
+        gui = "italic",
+    },
+    buffer_selected = {
+        gui = "bold",
+    },
+    tab_selected = {
+        guifg = {
+            attribute = "fg",
+            -- highlight = "Pmenu"
+            highlight = "BufferLineBufferSelected"
+        },
+        gui = "bold",
+    },
 }
