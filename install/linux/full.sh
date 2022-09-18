d5d334ce0ad621e162e3387063d72f08bcf9fce2
Serhat Teker <serhat.teker@gmail.com>
2022-09-18 22:26:23 +0300

    Add initial linux full install script

---
+++ install/linux/full.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# vim: set ft=sh et ts=4 sw=4 sts=4:
+# ----------------------------------------------------------------------------#
+#                         _                          _
+#                        | |__   __ _ ___  ___   ___| |__
+#                        | '_ \ / _` / __|/ _ \ / __| '_ \
+#                        | |_) | (_| \__ \  __/_\__ \ | | |
+#                        |_.__/ \__,_|___/\___(_)___/_| |_|
+#
+# Author: Serhat Teker <serhat.teker@gmail.com>
+# Source: https://github.com/SerhatTeker/dotfiles
+#
+# ----------------------------------------------------------------------------#
+#
+# Bash safeties: exit on error, no unset variables, pipelines can't hide errors
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Locate the root directory
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+# shellcheck disable=1091
+source "${ROOT}/common.sh"
+
+# Applications {{{
+
+install_apts() {
+    sudo apt install -y \
+        gparted
+}
+
+install_snaps() {
+    sudo snap install multipass
+}
+# }}}
+
+main() {
+    info "Started base ${OSTYPE}"
+    sudo apt update
+    install_apts
+    install_snaps
+
+    success "Finished full ${OSTYPE}"
+}
+
+main
