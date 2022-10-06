48a9750b194b69b7da5901380f8f6f0d54af6223
Serhat Teker <serhat.teker@gmail.com>
2022-10-06 12:45:00 +0300

    Add brew macos full file

--- install/macos/brew.sh
+++ install/macos/brew.sh
@@ -2,38 +2,49 @@
 # -*- coding: utf-8 -*-
 # vim: set ft=sh et ts=4 sw=4 sts=4:
 
-
 # Install brew and bundle
 # Usage:
 # $ bash brew.sh
-
 
 # Bash safeties: exit on error, no unset variables, pipelines can't hide errors
 set -o errexit
 set -o nounset
 set -o pipefail
 
-
 # Locate the root directory
-ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-# shellcheck source=scripts/common.sh
+# shellcheck disable=1091
 source "${ROOT}/common.sh"
 
-
 install_brew() {
+    # If brew presents
     command_exists brew && return
 
+    # If ~/.homebrew presents
+    [[ -d "${HOME}/.homebrew" ]] && return
+
     # Install Homebrew to $HOME/.homebrew instead of /usr/local:
-    git clone https://github.com/Homebrew/brew.git ${HOME}/.homebrew
+    git clone https://github.com/Homebrew/brew.git "${HOME}/.homebrew"
+
+    # Setup install
+    cd "${HOME}/" &&
+        eval "$(.homebrew/bin/brew shellenv)"
+    brew update --force --quiet
+    chmod -R go-w "$(brew --prefix)/share/zsh"
+
     # check brew installed
     which brew
+
     # close analytics
     brew analytics off
+
+    # install cask
+    brew install cask
 }
 
-brew_bundle() {
-    brew bundle install --file="${ROOT}/Brewfile"
+brew_bundle_base() {
+    brew bundle install --file="${ROOT}/install/macos/Brewfile.base.macos"
 }
 
 # NOTE: Disabled
@@ -45,9 +56,10 @@ ictags() {
     brew install --HEAD universal-ctags
 }
 
+# TODO: Add full opt and func
 main() {
     install_brew
-    brew_bundle
+    brew_bundle_base
 }
 
 main "$@"
