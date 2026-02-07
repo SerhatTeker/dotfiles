72e990d218c233e8c83ac631ebe3343dc8d251ef
bumper-serhat-teker <114921752+bumper-serhat-teker@users.noreply.github.com>
2026-02-08 01:50:27 +0300

    Update install/tmux.sh

--- install/tmux.sh
+++ install/tmux.sh
@@ -25,42 +25,42 @@ ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 source "${ROOT}/install/common.sh"
 
 install_tmux() {
-    # if tmux already installed skip
+    # if tmux already installed skip install
     command_exists tmux && return
-
-    is_linux && {
-        sudo apt install tmux
-        return 0
-    }
 
     # install with brew but check brew installed first
     if ! command_exists brew; then
-        msg_cli red "brew not installed, first you need to install brew!" normal
-        msg_cli yellow "Use your os brew.sh script" normal
-        msg_cli yellow "Or follow https://docs.brew.sh/Installation" normal
+        error "brew not installed, first you need to install brew!"
+        warn "Use your os brew.sh script"
+        warn "Or follow https://docs.brew.sh/Installation"
         exit 1
     else
         brew install tmux
     fi
 }
 
-# Install tmux tpm and plugins
+# Install tmux, tpm and plugins
 plugins() {
     local target="${XDG_DATA_HOME}/tmux/plugins"
+
+    # TODO: Check if the dir exists delete it
+    rm -rf "${target}"
     mkdir -p "${target}"
     git clone https://github.com/tmux-plugins/tpm "${target}/tpm"
 
+    # NOTE: This will be done automatically when opening a tmux instance
+    #
+    # INFO: We must inside tmux to run this
     # Reload TMUX environment so TPM is sourced:
-    tmux source "${XDG_CONFIG_HOME}/tmux/tmux.conf"
-
+    # tmux source "${XDG_CONFIG_HOME}/tmux/tmux.conf"
     # Install plugins
-    "${target}/tpm/bin/install_plugins"
+    # "${target}/tpm/bin/install_plugins"
 }
 
 main() {
     install_tmux
-    force_remove "${DOTFILES}/tmux" "${XDG_CONFIG_HOME}/tmux" # link config. overwrites link.sh
+    force_remove "${DOTFILES}/tmux" "${XDG_CONFIG_HOME}/tmux"
     plugins
 }
 
-main "$@"
+main
