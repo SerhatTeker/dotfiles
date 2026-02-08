c6756243fdea4fc2d02db838c0b968874929e2fa
bumper-serhat-teker <114921752+bumper-serhat-teker@users.noreply.github.com>
2026-02-08 15:37:03 +0300

    Update install base.sh

--- install/base.sh
+++ install/base.sh
@@ -23,15 +23,7 @@ ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 # shellcheck disable=1091
 source "${ROOT}/install/common.sh"
 
-# TODO: Add bash-language-server install
-os_base() {
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        bash "${ROOT}/install/linux/base.sh"
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        bash "${ROOT}/install/macos/base.sh"
-    fi
-}
-
+# TODO: Check still needed
 # TODO: Check for whether VM or PC
 apps_pc() {
     local dir="${1}"
@@ -40,51 +32,29 @@ apps_pc() {
     # bash "${dir}/apps/gopass.sh" # INFO: disabled since gnupg dep is pain
 }
 
-apps() {
-    info "Started apps installation"
-
-    mkdir -p "${HOME}/apps"
-
-    local dir="${ROOT}/install"
-    bash "${dir}/apps/alacritty.sh"
-
-    # apps_pc "${dir}"
-
-    # rclone
-    curl https://rclone.org/install.sh | sudo bash
-}
-
-git_skip_os_depend() {
-    # Skip alacritty which made from toggle-theme bin
-    git update-index --skip-worktree \
-        "${ROOT}/alacritty/alacritty.yml" \
-        "${ROOT}/alacritty/colors/gruvbox.yml" \
-        "${ROOT}/alacritty/colors/one.yml"
-}
-
-main() {
-    info "Started base installation"
-
-    sudo -v # Get sudo beforehand
-
-    os_base
-    apps
-
+install_lib_and_apps() {
     declare -a installs=(
         "zsh.sh"
         "link.sh"
         "tmux.sh"
         "languages/python.sh"
-        "languages/node.sh"
         "languages/rust.sh"
-        "nvim.sh"
-        "lvim.sh"
+        # "languages/node.sh"
+        "nvchad.sh"
         "fonts.sh"
     )
 
     for install in "${installs[@]}"; do
         bash "${ROOT}/install/${install}"
     done
+}
+
+main() {
+    info "Started base installation"
+
+    sudo -v # Get sudo beforehand
+    bash "${ROOT}/install/macos/base.sh"
+    install_lib_and_apps
 
     success "Finished base installation"
 }
