3535719c7ebb31eb3ecab7a6a16db8c7bd3fa7b3
bumper-serhat-teker <114921752+bumper-serhat-teker@users.noreply.github.com>
2026-02-22 17:16:41 +0300

    Add bufferline config

---
+++ lazyvim/lua/plugins/bufferline.lua
@@ -0,0 +1,27 @@
+M = {
+  {
+    "akinsho/bufferline.nvim",
+    -- LazyVim will deep merge this `opts` table with its own default `opts`
+    opts = {
+      options = {
+        diagnostics = false,
+        buffer_close_icon = "",
+        close_icon = "",
+        indicator = {
+          style = "icon",
+          icon = "",
+        },
+      },
+      highlights = {
+        background = {
+          italic = true,
+        },
+        buffer_selected = {
+          bold = true,
+        },
+      },
+    },
+  },
+}
+
+return M
