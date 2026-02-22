49017c5f2b3f7965328c2d5d847354206f954449
bumper-serhat-teker <114921752+bumper-serhat-teker@users.noreply.github.com>
2026-02-22 22:34:46 +0300

    Add python and root_spec options

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- LSP Server to use for Python.
+vim.g.lazyvim_python_lsp = "pyright"
+-- Set to "ruff_lsp" to use the old LSP implementation version.
+-- vim.g.lazyvim_python_ruff = "ruff"
+
+-- LazyVim root dir detection
+-- Each entry can be:
+-- * the name of a detector function like `lsp` or `cwd`
+-- * a pattern or array of patterns like `.git` or `lua`.
+-- * a function with signature `function(buf) -> string|string[]`
+vim.g.root_spec = { ".git", "lsp", "cwd" }
