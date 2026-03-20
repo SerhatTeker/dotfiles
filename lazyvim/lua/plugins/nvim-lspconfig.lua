65a261103e338d37667f6125b1f90eb84d811ca3
bumper-serhat-teker <114921752+bumper-serhat-teker@users.noreply.github.com>
2026-03-20 22:52:41 +0300

    Fix nvim-lspconfig sign issue

--- lazyvim/lua/plugins/nvim-lspconfig.lua
+++ lazyvim/lua/plugins/nvim-lspconfig.lua
@@ -5,12 +5,6 @@ M = {
       diagnostics = {
         -- completely disables the inline error/warning text
         virtual_text = false,
-
-        -- Keeps the signs (icons) in the far left gutter
-        signs = true,
-
-        -- Keeps underlines on the code with the error
-        underline = true,
       },
     },
   },
